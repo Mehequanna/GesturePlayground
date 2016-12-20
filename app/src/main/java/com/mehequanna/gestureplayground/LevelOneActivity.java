a53da22491a0c16642832ef9d2c7164cff44b6cf
Stephen Emery <emery.stephen@gmail.com>
2016-12-20 14:25:54 -0800

    Bind chicken ImageViews in Level One Activity.

--- app/src/main/java/com/mehequanna/gestureplayground/LevelOneActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/LevelOneActivity.java
@@ -2,12 +2,25 @@ package com.mehequanna.gestureplayground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LevelOneActivity extends AppCompatActivity {
+    @Bind(R.id.chicken0) ImageView mChicken0;
+    @Bind(R.id.chicken1) ImageView mChicken1;
+    @Bind(R.id.chicken2) ImageView mChicken2;
+    @Bind(R.id.chicken3) ImageView mChicken3;
+    @Bind(R.id.chicken4) ImageView mChicken4;
+    @Bind(R.id.chicken5) ImageView mChicken5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
+        ButterKnife.bind(this);
+
+
     }
 }
