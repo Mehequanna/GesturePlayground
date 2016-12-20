c231754c9fe393f9c6ee15d050e121e43507cc7e
Stephen Emery <emery.stephen@gmail.com>
2016-12-20 14:50:07 -0800

    Add OnTouchListener and make chickens 1 - 5 invisible.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/LevelOneActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelOneActivity.java
@@ -2,6 +2,9 @@ package com.mehequanna.gestureplayground.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.mehequanna.gestureplayground.R;
@@ -9,7 +12,7 @@ import com.mehequanna.gestureplayground.R;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class LevelOneActivity extends AppCompatActivity {
+public class LevelOneActivity extends AppCompatActivity implements View.OnTouchListener {
     @Bind(R.id.chicken0) ImageView mChicken0;
     @Bind(R.id.chicken1) ImageView mChicken1;
     @Bind(R.id.chicken2) ImageView mChicken2;
@@ -17,12 +20,25 @@ public class LevelOneActivity extends AppCompatActivity {
     @Bind(R.id.chicken4) ImageView mChicken4;
     @Bind(R.id.chicken5) ImageView mChicken5;
 
+    private GestureDetector mChicken0GestureDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
         ButterKnife.bind(this);
 
+        mChicken1.setVisibility(View.INVISIBLE);
+        mChicken2.setVisibility(View.INVISIBLE);
+        mChicken3.setVisibility(View.INVISIBLE);
+        mChicken4.setVisibility(View.INVISIBLE);
+        mChicken5.setVisibility(View.INVISIBLE);
 
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
     }
 }
