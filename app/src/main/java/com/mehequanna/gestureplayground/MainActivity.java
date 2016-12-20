6f1e46655012ddac5d038f294d619bfb7ac98ebb
Stephen Emery <emery.stephen@gmail.com>
2016-12-20 10:09:25 -0800

    Add onClickListeners to buttons in Main Activity.

--- app/src/main/java/com/mehequanna/gestureplayground/MainActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/MainActivity.java
@@ -1,14 +1,16 @@
 package com.mehequanna.gestureplayground;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.levelOneImageButton) ImageButton mLevelOneImageButton;
     @Bind(R.id.parentsButton)
     Button mParentsButton;
@@ -19,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mLevelOneImageButton.setOnClickListener(this);
+        mParentsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLevelOneImageButton) {
+            Intent intent = new Intent(MainActivity.this, LevelOneActivity.class);
+            startActivity(intent);
+        }
+        if (view == mParentsButton) {
+            Intent intent = new Intent(MainActivity.this, ParentsActivity.class);
+            startActivity(intent);
+        }
     }
 }
