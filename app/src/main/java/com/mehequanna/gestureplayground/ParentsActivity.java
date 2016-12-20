d798d277d8bb49acb7f6051803342dbf03854163
Stephen Emery <emery.stephen@gmail.com>
2016-12-20 10:08:14 -0800

    Add Parents Activity.

---
+++ app/src/main/java/com/mehequanna/gestureplayground/ParentsActivity.java
@@ -0,0 +1,13 @@
+package com.mehequanna.gestureplayground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parents);
+    }
+}
