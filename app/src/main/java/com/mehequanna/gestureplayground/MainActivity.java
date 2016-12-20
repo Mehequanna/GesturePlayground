b5f3651d4fd05fa0dc2cd3ea6649354c9ded69ef
Stephen Emery <emery.stephen@gmail.com>
2016-12-20 10:02:02 -0800

    Bind buttons in Main Activity.

--- app/src/main/java/com/mehequanna/gestureplayground/MainActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/MainActivity.java
@@ -2,12 +2,22 @@ package com.mehequanna.gestureplayground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.levelOneImageButton) ImageButton mLevelOneImageButton;
+    @Bind(R.id.parentsButton)
+    Button mParentsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
