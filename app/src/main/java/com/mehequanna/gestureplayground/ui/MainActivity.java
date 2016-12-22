fd0681d9fc744bceeac93be303b14debe1ce9b12
Stephen Emery <emery.stephen@gmail.com>
2016-12-22 10:23:05 -0800

    Add start video in onCreate.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/MainActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package com.mehequanna.gestureplayground.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.VideoView;
 
 import com.mehequanna.gestureplayground.R;
 
@@ -14,14 +16,18 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.levelOneImageButton) ImageButton mLevelOneImageButton;
-    @Bind(R.id.parentsButton)
-    Button mParentsButton;
+    @Bind(R.id.parentsButton) Button mParentsButton;
+    @Bind(R.id.mainVideoView) VideoView mMainVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.maintap);
+        mMainVideoView.setVideoURI(uri);
+        mMainVideoView.start();
 
         mLevelOneImageButton.setOnClickListener(this);
         mParentsButton.setOnClickListener(this);
