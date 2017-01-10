b9770501d6a26626aa5a555ab0726eb97cb792c6
Stephen Emery <emery.stephen@gmail.com>
2017-01-09 17:29:23 -0800

    Remove MainTap video on Main Activity Layout and .java.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/MainActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/MainActivity.java
@@ -1,33 +1,25 @@
 package com.mehequanna.gestureplayground.ui;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import com.mehequanna.gestureplayground.R;
-import com.mehequanna.gestureplayground.util.DetectGestures;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.levelOneImageButton) ImageButton mLevelOneImageButton;
     @Bind(R.id.levelTwoImageButton) ImageButton mLevelTwoImageButton;
     @Bind(R.id.levelThreeImageButton) ImageButton mLevelThreeImageButton;
     @Bind(R.id.levelFourImageButton) ImageButton mLevelFourImageButton;
     @Bind(R.id.levelFiveImageButton) ImageButton mLevelFiveImageButton;
     @Bind(R.id.parentsButton) Button mParentsButton;
-    @Bind(R.id.mainVideoView) VideoView mMainVideoView;
-
-    private GestureDetector mVideoGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,26 +27,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.maintap);
-        mMainVideoView.setVideoURI(uri);
-        mMainVideoView.start();
-
-        mVideoGestureDetector = new GestureDetector(this, new DetectGestures() {
-            @Override
-            public boolean onDown(MotionEvent e) {
-                mMainVideoView.stopPlayback();
-                mMainVideoView.setVisibility(View.GONE);
-                return true;
-            }
-        });
-
         mLevelOneImageButton.setOnClickListener(this);
         mLevelTwoImageButton.setOnClickListener(this);
         mLevelThreeImageButton.setOnClickListener(this);
         mLevelFourImageButton.setOnClickListener(this);
         mLevelFiveImageButton.setOnClickListener(this);
         mParentsButton.setOnClickListener(this);
-        mMainVideoView.setOnTouchListener(this);
     }
 
     @Override
@@ -88,14 +66,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(MainActivity.this, ParentsActivity.class);
             startActivity(intent);
         }
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        if (view == mMainVideoView) {
-            mVideoGestureDetector.onTouchEvent(motionEvent);
-            return true;
-        }
-        return false;
     }
 }
