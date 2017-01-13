614e2f1cd2064a9e88412013bc479b8bcbecce8d
Stephen Emery <emery.stephen@gmail.com>
2017-01-13 13:58:37 -0800

    Add binds and onTouchListeners to cow ImageViews.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/LevelOneActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelOneActivity.java
@@ -2,14 +2,47 @@ package com.mehequanna.gestureplayground.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.mehequanna.gestureplayground.R;
 
-public class LevelOneActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LevelOneActivity extends AppCompatActivity implements View.OnTouchListener {
+    @Bind(R.id.cowOne) ImageView mCowOne;
+    @Bind(R.id.cowTwo) ImageView mCowTwo;
+    @Bind(R.id.cowThree) ImageView mCowThree;
+    @Bind(R.id.cowFour) ImageView mCowFour;
+    @Bind(R.id.cowFive) ImageView mCowFive;
+    @Bind(R.id.cowSix) ImageView mCowSix;
+    @Bind(R.id.cowSeven) ImageView mCowSeven;
+    @Bind(R.id.cowEight) ImageView mCowEight;
+    @Bind(R.id.cowNine) ImageView mCowNine;
+    @Bind(R.id.cowTen) ImageView mCowTen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
+        ButterKnife.bind(this);
+
+        mCowOne.setOnTouchListener(this);
+        mCowTwo.setOnTouchListener(this);
+        mCowThree.setOnTouchListener(this);
+        mCowFour.setOnTouchListener(this);
+        mCowFive.setOnTouchListener(this);
+        mCowSix.setOnTouchListener(this);
+        mCowSeven.setOnTouchListener(this);
+        mCowEight.setOnTouchListener(this);
+        mCowNine.setOnTouchListener(this);
+        mCowTen.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
     }
 }
