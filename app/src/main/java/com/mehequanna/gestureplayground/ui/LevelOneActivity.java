3e42c0b15335d0d2ca610f0612427ce0ab74c046
Stephen Emery <emery.stephen@gmail.com>
2017-01-13 14:37:44 -0800

    Add member cow Gesture Detectors, setVisibility to cows after one, and created mCowOneGestureDetector.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/LevelOneActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelOneActivity.java
@@ -2,11 +2,13 @@ package com.mehequanna.gestureplayground.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.mehequanna.gestureplayground.R;
+import com.mehequanna.gestureplayground.util.DetectGestures;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,11 +25,41 @@ public class LevelOneActivity extends AppCompatActivity implements View.OnTouchL
     @Bind(R.id.cowNine) ImageView mCowNine;
     @Bind(R.id.cowTen) ImageView mCowTen;
 
+    private GestureDetector mCowOneGestureDetector;
+    private GestureDetector mCowTwoGestureDetector;
+    private GestureDetector mCowThreeGestureDetector;
+    private GestureDetector mCowFourGestureDetector;
+    private GestureDetector mCowFiveGestureDetector;
+    private GestureDetector mCowSixGestureDetector;
+    private GestureDetector mCowSevenGestureDetector;
+    private GestureDetector mCowEightGestureDetector;
+    private GestureDetector mCowNineGestureDetector;
+    private GestureDetector mCowTenGestureDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
         ButterKnife.bind(this);
+
+        mCowTwo.setVisibility(View.INVISIBLE);
+        mCowThree.setVisibility(View.INVISIBLE);
+        mCowFour.setVisibility(View.INVISIBLE);
+        mCowFive.setVisibility(View.INVISIBLE);
+        mCowSix.setVisibility(View.INVISIBLE);
+        mCowSeven.setVisibility(View.INVISIBLE);
+        mCowEight.setVisibility(View.INVISIBLE);
+        mCowNine.setVisibility(View.INVISIBLE);
+        mCowTen.setVisibility(View.INVISIBLE);
+
+        mCowOneGestureDetector = new GestureDetector(this, new DetectGestures(){
+           @Override
+            public boolean onSingleTapConfirmed(MotionEvent e){
+               mCowOne.setVisibility(View.INVISIBLE);
+               mCowTwo.setVisibility(View.VISIBLE);
+               return true;
+           }
+        });
 
         mCowOne.setOnTouchListener(this);
         mCowTwo.setOnTouchListener(this);
@@ -43,6 +75,11 @@ public class LevelOneActivity extends AppCompatActivity implements View.OnTouchL
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (view == mCowOne) {
+            mCowOneGestureDetector.onTouchEvent(motionEvent);
+            return true;
+        }
+
         return false;
     }
 }
