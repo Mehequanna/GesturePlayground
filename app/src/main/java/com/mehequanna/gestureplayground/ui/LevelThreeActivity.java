7ac02fee4298264a757f88abe50bc0cfc4a61590
Stephen Emery <emery.stephen@gmail.com>
2017-01-13 19:55:27 -0800

    Add test code for swipe right in level three.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/LevelThreeActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelThreeActivity.java
@@ -2,14 +2,50 @@ package com.mehequanna.gestureplayground.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.mehequanna.gestureplayground.R;
+import com.mehequanna.gestureplayground.util.DetectGestures;
 
-public class LevelThreeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LevelThreeActivity extends AppCompatActivity implements View.OnTouchListener {
+    @Bind(R.id.airplaneBlue) ImageView mAirplaneBlue;
+
+    private GestureDetector mAirplaneBlueGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_three);
+        ButterKnife.bind(this);
+
+        mAirplaneBlueGestureDetector = new GestureDetector(this, new DetectGestures() {
+            @Override
+            public void onSwipeRight() {
+                Toast.makeText(LevelThreeActivity.this, "swiped", Toast.LENGTH_SHORT).show();
+                Animation moveRight = AnimationUtils.loadAnimation(
+                        getApplicationContext(), R.anim.move_right_fade_animation);
+                mAirplaneBlue.startAnimation(moveRight);
+            }
+        });
+
+        mAirplaneBlue.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (view == mAirplaneBlue) {
+            mAirplaneBlueGestureDetector.onTouchEvent(motionEvent);
+            return true;
+        }
+        return false;
     }
 }
