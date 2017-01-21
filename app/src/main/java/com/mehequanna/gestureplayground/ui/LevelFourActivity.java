a7dc0b8789aa296475b85841d6114b4a2e131351
Stephen Emery <emery.stephen@gmail.com>
2017-01-20 22:38:33 -0800

    Remove toasts in LevelFourActivity and add fling_animation.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/LevelFourActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelFourActivity.java
@@ -9,7 +9,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.mehequanna.gestureplayground.R;
 import com.mehequanna.gestureplayground.util.DetectGestures;
@@ -43,47 +42,36 @@ public class LevelFourActivity extends AppCompatActivity implements View.OnTouch
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
                 if (mPig1.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "One!", Toast.LENGTH_SHORT).show();
                     mPig1.setVisibility(View.INVISIBLE);
                     mPig2.setVisibility(View.VISIBLE);
                 } else if (mPig2.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Two!", Toast.LENGTH_SHORT).show();
                     mPig2.setVisibility(View.INVISIBLE);
                     mPig3.setVisibility(View.VISIBLE);
                 } else if (mPig3.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Three!", Toast.LENGTH_SHORT).show();
                     mPig3.setVisibility(View.INVISIBLE);
                     mPig4.setVisibility(View.VISIBLE);
                 } else if (mPig4.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Four!", Toast.LENGTH_SHORT).show();
                     mPig4.setVisibility(View.INVISIBLE);
                     mPig5.setVisibility(View.VISIBLE);
                 } else if (mPig5.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Five!", Toast.LENGTH_SHORT).show();
                     mPig5.setVisibility(View.INVISIBLE);
                     mPig6.setVisibility(View.VISIBLE);
                 } else if (mPig6.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Six!", Toast.LENGTH_SHORT).show();
                     mPig6.setVisibility(View.INVISIBLE);
                     mPig7.setVisibility(View.VISIBLE);
                 } else if (mPig7.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Seven!", Toast.LENGTH_SHORT).show();
                     mPig7.setVisibility(View.INVISIBLE);
                     mPig8.setVisibility(View.VISIBLE);
                 } else if (mPig8.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Eight!", Toast.LENGTH_SHORT).show();
                     mPig8.setVisibility(View.INVISIBLE);
                     mPig9.setVisibility(View.VISIBLE);
                 } else if (mPig9.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Nine!", Toast.LENGTH_SHORT).show();
                     mPig9.setVisibility(View.INVISIBLE);
                     mPig10.setVisibility(View.VISIBLE);
                 } else if (mPig10.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Ten!", Toast.LENGTH_SHORT).show();
                     mPig10.setVisibility(View.INVISIBLE);
                     mPig11.setVisibility(View.VISIBLE);
                 } else if (mPig11.isShown()) {
-                    Toast.makeText(LevelFourActivity.this, "Eleven!", Toast.LENGTH_SHORT).show();
                     mPig11.setVisibility(View.INVISIBLE);
                     mWinTextView.setText(R.string.great_job);
 
