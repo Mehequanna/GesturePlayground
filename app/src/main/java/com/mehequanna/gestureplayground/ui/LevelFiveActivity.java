26f545264bb44c06f18a5dec12aea3064ad48ea3
Stephen Emery <emery.stephen@gmail.com>
2017-01-21 00:55:49 -0800

    Add initial files and settings for LevelFiveActivity.

---
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelFiveActivity.java
@@ -0,0 +1,15 @@
+package com.mehequanna.gestureplayground.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mehequanna.gestureplayground.R;
+
+public class LevelFiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_five);
+    }
+}
