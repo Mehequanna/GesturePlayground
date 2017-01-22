8a1e4cc6bb2de9fc20a626f6b2db26b91a3ed7d1
Stephen Emery <emery.stephen@gmail.com>
2017-01-21 17:38:19 -0800

    Add Gesture Detector and test it.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/LevelFiveActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelFiveActivity.java
@@ -2,12 +2,15 @@ package com.mehequanna.gestureplayground.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mehequanna.gestureplayground.R;
+import com.mehequanna.gestureplayground.util.DetectGestures;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +26,8 @@ public class LevelFiveActivity extends AppCompatActivity implements View.OnTouch
     @Bind(R.id.cowTen) ImageView mCowTen;
     @Bind(R.id.chickenPink) ImageView mChickenPink;
     @Bind(R.id.chickenBlue) ImageView mChickenBlue;
+
+    GestureDetector mGestureDetector;
 
     int mPigUpId;
     int mPigDownId;
@@ -51,6 +56,51 @@ public class LevelFiveActivity extends AppCompatActivity implements View.OnTouch
         mChickenPinkId = mChickenPink.getId();
         mChickenBlueId = mChickenBlue.getId();
 
+        mGestureDetector = new GestureDetector(this, new DetectGestures(){
+            @Override
+            public boolean onDown(MotionEvent e) {
+                if (mViewId == mAirplaneBlueId) {
+                    Toast.makeText(LevelFiveActivity.this, "Blue!", Toast.LENGTH_SHORT).show();
+                } else if (mViewId == mAirplaneRedId) {
+                    Toast.makeText(LevelFiveActivity.this, "Red", Toast.LENGTH_SHORT).show();
+                } else if (mViewId == mTractorRedId) {
+                    Toast.makeText(LevelFiveActivity.this, "Tractor", Toast.LENGTH_SHORT).show();
+                }
+                return super.onDown(e);
+            }
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                if (mViewId == mAirplaneBlueId || mViewId == mAirplaneRedId || mViewId == mTractorRedId) {
+                    Toast.makeText(LevelFiveActivity.this, "Right", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                if (mViewId == mAirplaneBlueId || mViewId == mAirplaneRedId || mViewId == mTractorRedId) {
+                    Toast.makeText(LevelFiveActivity.this, "Left", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                return super.onSingleTapConfirmed(e);
+            }
+
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                return super.onDoubleTap(e);
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return super.onFling(e1, e2, velocityX, velocityY);
+            }
+        });
+
         mPigUp.setOnTouchListener(this);
         mPigDown.setOnTouchListener(this);
         mAirplaneRed.setOnTouchListener(this);
@@ -65,6 +115,7 @@ public class LevelFiveActivity extends AppCompatActivity implements View.OnTouch
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         mViewId = view.getId();
-        return false;
+        mGestureDetector.onTouchEvent(motionEvent);
+        return true;
     }
 }
