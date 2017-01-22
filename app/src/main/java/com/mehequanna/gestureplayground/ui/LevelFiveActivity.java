4673bed01c5bc2fb67d83b39e0c06270c212c71e
Stephen Emery <emery.stephen@gmail.com>
2017-01-21 17:57:43 -0800

    Add moveRight and moveLeft animations and sounds onSwipe in LevelFiveActivity.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/LevelFiveActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/LevelFiveActivity.java
@@ -1,13 +1,15 @@
 package com.mehequanna.gestureplayground.ui;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.mehequanna.gestureplayground.R;
 import com.mehequanna.gestureplayground.util.DetectGestures;
@@ -40,6 +42,13 @@ public class LevelFiveActivity extends AppCompatActivity implements View.OnTouch
     int mChickenBlueId;
     int mViewId;
 
+    MediaPlayer planeStartLeft;
+    MediaPlayer planeStartRight;
+    MediaPlayer planeFlyRight;
+    MediaPlayer planeFlyLeft;
+    MediaPlayer tractorStartLeft;
+    MediaPlayer tractorDriveRight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,15 +65,22 @@ public class LevelFiveActivity extends AppCompatActivity implements View.OnTouch
         mChickenPinkId = mChickenPink.getId();
         mChickenBlueId = mChickenBlue.getId();
 
+        planeStartLeft = MediaPlayer.create(this, R.raw.planestartleft);
+        planeStartRight = MediaPlayer.create(this, R.raw.planestartright);
+        planeFlyLeft = MediaPlayer.create(this, R.raw.planeflytoleft);
+        planeFlyRight = MediaPlayer.create(this, R.raw.planeflytoright);
+        tractorStartLeft = MediaPlayer.create(this, R.raw.tractorstartleft);
+        tractorDriveRight = MediaPlayer.create(this, R.raw.tractordrivingtoright);
+
         mGestureDetector = new GestureDetector(this, new DetectGestures(){
             @Override
             public boolean onDown(MotionEvent e) {
                 if (mViewId == mAirplaneBlueId) {
-                    Toast.makeText(LevelFiveActivity.this, "Blue!", Toast.LENGTH_SHORT).show();
+                    planeStartLeft.start();
                 } else if (mViewId == mAirplaneRedId) {
-                    Toast.makeText(LevelFiveActivity.this, "Red", Toast.LENGTH_SHORT).show();
+                    planeStartRight.start();
                 } else if (mViewId == mTractorRedId) {
-                    Toast.makeText(LevelFiveActivity.this, "Tractor", Toast.LENGTH_SHORT).show();
+                    tractorStartLeft.start();
                 }
                 return super.onDown(e);
             }
@@ -72,16 +88,24 @@ public class LevelFiveActivity extends AppCompatActivity implements View.OnTouch
             @Override
             public void onSwipeRight() {
                 super.onSwipeRight();
-                if (mViewId == mAirplaneBlueId || mViewId == mAirplaneRedId || mViewId == mTractorRedId) {
-                    Toast.makeText(LevelFiveActivity.this, "Right", Toast.LENGTH_SHORT).show();
+                if (mViewId == mTractorRedId) {
+                    tractorStartLeft.stop();
+                    tractorDriveRight.start();
+                    moveRight(mTractorRed);
                 }
             }
 
             @Override
             public void onSwipeLeft() {
                 super.onSwipeLeft();
-                if (mViewId == mAirplaneBlueId || mViewId == mAirplaneRedId || mViewId == mTractorRedId) {
-                    Toast.makeText(LevelFiveActivity.this, "Left", Toast.LENGTH_SHORT).show();
+                if (mViewId == mAirplaneBlueId) {
+                    planeStartLeft.start();
+                    planeFlyRight.start();
+                    moveRight(mAirplaneBlue);
+                } else if (mViewId == mAirplaneRedId) {
+                    planeStartRight.start();
+                    planeFlyLeft.start();
+                    moveLeft(mAirplaneRed);
                 }
             }
 
@@ -117,5 +141,17 @@ public class LevelFiveActivity extends AppCompatActivity implements View.OnTouch
         mViewId = view.getId();
         mGestureDetector.onTouchEvent(motionEvent);
         return true;
+    }
+
+    public void moveRight(View view) {
+        Animation moveRight = AnimationUtils.loadAnimation(
+                getApplicationContext(), R.anim.move_right_fade_animation);
+        view.startAnimation(moveRight);
+    }
+
+    private void moveLeft(View view) {
+        Animation moveLeft = AnimationUtils.loadAnimation(
+                getApplicationContext(), R.anim.move_left_fade_animation);
+        view.startAnimation(moveLeft);
     }
 }
