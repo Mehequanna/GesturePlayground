e0583737eba174f2027864c268ef5cfafd5334c9
Stephen Emery <emery.stephen@gmail.com>
2017-02-27 21:30:29 -0800

    Add transitions to up button in parent activity.

--- app/src/main/java/com/mehequanna/gestureplayground/ui/ParentsActivity.java
+++ app/src/main/java/com/mehequanna/gestureplayground/ui/ParentsActivity.java
@@ -2,6 +2,7 @@ package com.mehequanna.gestureplayground.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.mehequanna.gestureplayground.R;
 
@@ -11,5 +12,30 @@ public class ParentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parents);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.home:
+                onBackPressed();
+                break;
+            case R.id.homeAsUp:
+                onBackPressed();
+                break;
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
